0a7efe507551de30945fe42872b1d34f2ca8aad0
evamerica0442 <104032355+evamerica0442@users.noreply.github.com>
2025-07-27 18:54:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -683,4 +683,4 @@ if __name__ == '__main__':
         traceback.print_exc()
         exit(1)
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=10000)
